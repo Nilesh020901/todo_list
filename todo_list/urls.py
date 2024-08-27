ded48dbfaff1f5fc27f58673e3f330269f48ed88
Nilesh020901 <nileshsoni0209@gmail.com>
2024-08-27 13:15:51 +0530

    work on urls, models, views in office

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views
 
 urlpatterns = [
+    # Home Page #
+    path('', views.index, name="todo"),
+    # give id no. item_id name or item_id=i.id 
+    # pass item_id as primary key to remove that the todo with given id
+    path('del/<str:item_id>', views.remove, name="del"),
     path('admin/', admin.site.urls),
 ]
